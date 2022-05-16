126c65dc126c0937b92c8f20d1498ff547d1271f
Weslen <weslen1905@gmail.com>
2022-05-16 01:27:21 -0300

    Page

--- src/app/modules/gifs/gifs.routing.ts
+++
@@ -1,4 +0,0 @@
-import { GifsListComponent } from './list/list.component';
-import { Routes } from '@angular/router';
-
-export const gifsRoutes: Routes = [{ path: '', component: GifsListComponent }];
