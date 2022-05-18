2336aea390eac23c003021f6af349b0bb4bd0141
Weslen <weslen1905@gmail.com>
2022-05-18 13:55:21 -0300

    Toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { WalletService } from './core/services/wallet.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'samuel';
+export class AppComponent implements OnInit {
+  constructor(private _walletService: WalletService, private _router: Router) {
+
+  }
+  account: any = {
+    wallet: '',
+    balance: ''
+  };
+
+  ngOnInit(): void {
+    this._walletService.isAccountConnected().then((subject: any) => {
+      subject.subscribe((wallet: string) => { this.account.wallet = wallet; if (wallet) this._router.navigateByUrl('/gifs') })
+    })
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { WinRefService } from './core/services/winref.service';
 import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConnectComponent } from './core/components/connect/connect.component';
+
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+const material: any[] = [MatToolbarModule, MatProgressBarModule, MatButtonModule, MatIconModule]
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, ConnectComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, material],
   providers: [WinRefService],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
